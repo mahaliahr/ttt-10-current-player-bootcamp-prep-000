5cbfda5f5467084ed33bccbf0167a854c78dcc11
mahaliahr <mahalia_hr@outlook.com>
2018-08-21 14:46:19 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,26 @@
+=begin
+iterate through array to check if contains "x" or "o"
+if contains "x" or "o" increment the counter
+=end 
+
+def turn_count(board)
+  counter = 0 
+  board.each do |x| 
+  if x == "X"
+    puts "#{counter}"
+    counter += 1
+end 
+end
+
+board.each do |x| 
+  if x == "O"
+    puts "#{counter}"
+    counter += 1
+end 
+end 
+return counter
+end
+
+def current_player(board)
+  turn_count
+end 
